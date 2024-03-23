10e963637d99bfd7078136bb121d0e0e4361bd10
Nikita Dubyk <nikitad@guardianvets.com>
2024-03-23 15:58:43 +0300

    added new sections

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 import "./style.scss";
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+  smoothLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = smoothLink.getAttribute("href");
+
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
