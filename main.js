130736eaec262c819aac9663d0c26569e793137f
Nikita Dubyk <nikita_dubyk@mail.ru>
2024-04-04 19:02:07 +0300

    add header adaptive

--- main.js
+++ main.js
@@ -14,11 +14,25 @@ for (let smoothLink of smoothLinks) {
   });
 }
 
-const menu = document.querySelector(".header-menu");
-const info = document.querySelector(".header-info");
+const menu = document.querySelector(".header-menu-mobile");
 const burger = document.querySelector(".header-burger");
-burger.addEventListener("click", () => {
+burger.addEventListener("click", (event) => {
   burger.classList.toggle("header-burger-active");
-  menu.classList.toggle("header-menu-active");
-  info.classList.toggle("header-info-active");
+  menu.classList.toggle("header-menu-mobile-active");
+  if (menu.classList.contains("header-menu-mobile-active")) {
+    document.body.style.overflowY = "hidden";
+  } else {
+    document.body.removeAttribute("style");
+  }
 });
+
+const links = Array.from(menu.children);
+
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+
+function closeOnClick() {
+  burger.classList.remove("header-burger-active");
+  menu.classList.remove("header-menu-mobile-active");
+}
