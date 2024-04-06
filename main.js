db6baf8fd83a2a61838ff194f1b1c5abc220f2d2
Nikita Dubyk <nikita_dubyk@mail.ru>
2024-04-06 17:36:20 +0300

    start added send message to telegram, add modal

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+import MicroModal from "micromodal";
+
+import "./modal.scss";
 import "./style.scss";
 import "./media.scss";
+
+MicroModal.init({
+  disableScroll: true,
+  disableFocus: false,
+  awaitOpenAnimation: true,
+  awaitCloseAnimation: true,
+});
 
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
 for (let smoothLink of smoothLinks) {
@@ -16,7 +26,7 @@ for (let smoothLink of smoothLinks) {
 
 const menu = document.querySelector(".header-menu-mobile");
 const burger = document.querySelector(".header-burger");
-burger.addEventListener("click", (event) => {
+burger.addEventListener("click", () => {
   burger.classList.toggle("header-burger-active");
   menu.classList.toggle("header-menu-mobile-active");
   if (menu.classList.contains("header-menu-mobile-active")) {
@@ -33,6 +43,17 @@ links.forEach((link) => {
 });
 
 function closeOnClick() {
+  document.body.removeAttribute("style");
   burger.classList.remove("header-burger-active");
   menu.classList.remove("header-menu-mobile-active");
 }
+
+const button = document.querySelector("#send");
+button.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const TELEGRAM_KEY = import.meta.env.VITE_TELEGRAM_KEY;
+  const CHAT_ID = import.meta.env.VITE_CHAT_ID;
+  await fetch(
+    `https://api.telegram.org/bot${TELEGRAM_KEY}/sendMessage?chat_id=${CHAT_ID}&text=test`
+  );
+});
